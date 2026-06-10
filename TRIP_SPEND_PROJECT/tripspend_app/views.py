127f2bd927f69ec892e1e87da948717f32c7c23c
rithinrose1221-crypto <rithinrose1221@gmail.com>
2026-06-10 15:04:00 +0530

    small change in backend

--- TRIP_SPEND_PROJECT/tripspend_app/views.py
+++ TRIP_SPEND_PROJECT/tripspend_app/views.py
@@ -33,7 +33,7 @@ def signup_view(request):
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST2.get("username")
+        username = request.POST.get("username")
         password = request.POST.get("password")
 
         user = authenticate(request, username=username, password=password)
